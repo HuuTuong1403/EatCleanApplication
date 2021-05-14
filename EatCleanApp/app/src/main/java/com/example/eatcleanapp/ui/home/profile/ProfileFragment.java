cb5e7507a737097d722b4bb70c3e8a8e892f6533
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2021-05-14 20:18:48 +0700

    Update and fix bug

--- EatCleanApp/app/src/main/java/com/example/eatcleanapp/ui/home/profile/ProfileFragment.java
+++ EatCleanApp/app/src/main/java/com/example/eatcleanapp/ui/home/profile/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.eatcleanapp.ui.home.profile;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -25,6 +26,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.back24);
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         return view;
