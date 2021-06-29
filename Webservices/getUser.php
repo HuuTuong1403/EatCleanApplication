28aa3ae5a2b61475da4ce72dd187ca08950c56ef
HoaiThu0801 <dangnguyenhoaithu08012000@gmail.com>
2021-06-29 14:31:42 +0700

    fix user

--- Webservices/getUser.php
+++ Webservices/getUser.php
@@ -6,7 +6,7 @@
 	$data = mysqli_query($connect, $query);
 
 	class users{
-		function __construct($IDUser, $Email, $Password, $FullName, $Image, $LoginFB, $IDRole, $Username ){
+		function __construct($IDUser, $Email, $Password, $FullName, $Image, $LoginFB, $IDRole, $Username, $createTime, $Status){
 			$this -> IDUser = $IDUser;
 			$this -> Email = $Email;
 			$this -> Password = $Password;
@@ -14,14 +14,16 @@
 			$this -> Image = $Image;
 			$this -> LoginFB = $LoginFB;
 			$this -> IDRole = $IDRole;
-			$this -> Username = $Username;			
+			$this -> Username = $Username;	
+			$this -> createTime = $createTime;
+			$this -> Status = $Status;			
 		}
 	}
 	$users_array = array();
 
 	//Thêm phần tử vào màng
 	while ($row = mysqli_fetch_assoc($data)){
-		array_push($users_array, new users($row ['IDUser'], $row ['Email'], $row ['Password'],  $row ['FullName'], $row ['Image'], $row ['LoginFB'], $row ['IDRole'], $row ['Username'] ));
+		array_push($users_array, new users($row ['IDUser'], $row ['Email'], $row ['Password'],  $row ['FullName'], $row ['Image'], $row ['LoginFB'], $row ['IDRole'], $row ['Username'], $row ['createTime'], $row ['Status'] ));
 	}
 
 	//Chuyển định dạng mảng qua json
